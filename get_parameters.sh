f0e985b7ac12849e928ac4be437dcf3aacf915f5
wu.tianlong <wu.tianlong@immomo.com>
2018-12-17 17:55:21 +0800

    get script parameters

---
+++ get_parameters.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#  ./get_parameters.sh  1  2  3 4 
+
+echo " 参数总数 =  $#   "  
+
+echo " 文件名字 $0"
+echo " 第一个参数 $1"
+echo " 第二个参数 $2"
+echo " 第三个参数 $3"
+
+
+
+ # 总数 =  4   
+ # 文件名字 ./get_parameters.sh
+ # 第一个参数 1
+ # 第二个参数 2
+ # 第三个参数 3
+
+
